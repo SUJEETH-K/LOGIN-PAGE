94d280392473d59f7bc89b0e2b768d365269ac3b
SUJEETH KANDASAMY <109455611+SUJEETH-K@users.noreply.github.com>
2023-01-28 22:12:16 +0530

    Add files via upload

---
+++ constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color.fromARGB(255, 106, 110, 108);
+const kPrimaryLightColor = Color.fromARGB(255, 201, 197, 197);
+
+const double defaultPadding = 16.0;
